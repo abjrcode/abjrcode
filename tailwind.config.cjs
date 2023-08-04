3441e63a54e7aeb59d3031fc7942b4fb72739f37
Ibrahim Najjar <ABnajjar@hotmail.com>
2023-08-04 22:41:07 +0200

    Keep targeting mobile phones while also supporting print A4 size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['index.html'],
   theme: {
+    screens: {
+      xs: '595px',
+      ...defaultTheme.screens,
+    },
     container: {
       center: true,
       padding: '2rem',
